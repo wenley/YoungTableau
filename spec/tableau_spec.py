001f3ddab52a93ab2f6626092656e042bbb2e779
Wenley Tong <wenley.tong@gmail.com>
2016-01-09 11:42:37 -0800

    Tweaks to first test file.

--- spec/tableau_spec.py
+++ spec/tableau_spec.py
@@ -1,6 +1,9 @@
 from tableau import *
 from expects import *
 
-with describe('Tableau'):
+with describe(Tableau):
   with it('can make a Tableau'):
     expect(lambda: Tableau([1], [1])).not_to(raise_error(Exception))
+
+  with context('invalid arguments'):
+    pass
